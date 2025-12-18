2450e27a30843475a87acdbd84b0f11d2393b2e8
Sanchi <pvtsanchi@gmail.com>
2025-12-18 15:54:42 +0530

    Update and rename char_pattaern.java to arr_traversal.java

--- arr_traversal.java
+++ arr_traversal.java
@@ -1,13 +1,14 @@
-public class char_pattaern {
+public class arr_traversal {
     public static void main (String arg [])
     {
-        int [] arr = new arr [5];
-        arr [] = {1,2,3,4,5};
+        int [] arr = {1,2,3,4,5};
+ 
         for (int i = 0; i<arr.length; i++)
             {
                 System.out.println (arr[i]);
             }
     }
 }
+
 
 
