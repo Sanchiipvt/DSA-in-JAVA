37082281f5b9695101d91901f28595861f94f3d2
Sanchi <pvtsanchi@gmail.com>
2025-12-18 15:50:26 +0530

    Correct array handling in char_pattaern.java
    Fix array declaration and initialization in main method.

--- char_pattaern.java
+++ char_pattaern.java
@@ -1,11 +1,13 @@
 public class char_pattaern {
     public static void main (String arg [])
     {
-        int arr = new arr [5];
+        int [] arr = new arr [5];
+        arr [] = {1,2,3,4,5};
         for (int i = 0; i<arr.length; i++)
             {
                 System.out.println (arr[i]);
             }
     }
 }
+
 
