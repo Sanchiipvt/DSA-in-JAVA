c86d1ba8e342dbdec2c48e728b00e405a1bdba65
Sanchi <pvtsanchi@gmail.com>
2025-12-18 15:47:08 +0530

    Update char_pattaern.java

--- char_pattaern.java
+++ char_pattaern.java
@@ -1,6 +1,11 @@
 public class char_pattaern {
     public static void main (String arg [])
     {
-        
+        int arr = new arr [5];
+        for (int i = 0; i<arr.length; i++)
+            {
+                System.out.println (arr[i]);
+            }
     }
 }
+
