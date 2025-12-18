272c3efe5c1aa7fa55761e2682047b24f9291425
Sanchi <pvtsanchi@gmail.com>
2025-12-18 16:30:08 +0530

    Create evn_traversal.java

---
+++ evn_traversal.java
@@ -0,0 +1,23 @@
+public class rev_traversal
+{
+  public static void even_traversal (int [] arr)
+  {
+    for (int i = 0; i< arr.length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            System.out.println (arr[i]);
+        }
+        
+    }
+  }
+  public static void main (String args[])
+  {
+    int ar [] = {0,1,2,3,4,5,6};
+    for (int i = 0; i < ar.length; i++)
+    {
+      System.out.println (ar[i]);
+    }
+    even_traversal(ar);
+  }
+}
